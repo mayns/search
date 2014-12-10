dc12dd5ced71e3a78fd6e5bf155ac5e676323d96
dmitrymakhotin <surfpandas@gmail.com>
2014-12-10 20:16:18 +0300

    substring search

--- index/index.py
+++ index/index.py
@@ -6,20 +6,17 @@ import codecs
 from normal_form import get_normal
 import re
 
-
+delete = re.compile(r'\W+?|\d+?', re.UNICODE)
+clr = re.compile(r'\s+', re.UNICODE)
 
 def do_index(l):
-    """создает индекс. на вход - лист строк, каждая строка - отдельное четверостишье"""
-    # delete = re.compile(u'\W+?|\d+?', re.UNICODE)
+    """создает индекс. на вход - лист строк, каждая строка - отдельное четверостишье.
+    возвращает словарь {normal_form : [(poem number, position in poem, part of speech)]}"""
     poem_index = {}
     for i in xrange(len(l)):
         print 'creating index for poem ...', i
         pos = 0
-        # sen = delete.sub(' ', l[i])
-        # sen = sen.split()
-        sen = l[i].split()
-        # for s in sen:
-        #     print s
+        sen = clear(l[i]).split()
         for word in sen:
             normal = get_normal(word)
             for item in normal:
@@ -29,8 +26,14 @@ def do_index(l):
             pos +=1
     return poem_index
 
-delete = re.compile(r'\W+?|\d+?', re.UNICODE)
-pattern = re.compile(r'\s+', re.UNICODE)
+def clear(s):
+    """очищает строку и приводит к нижнему регистру"""
+    s = delete.sub(' ', s)
+    s = clr.sub(u' ', s).strip()
+    return s.lower()
+
+
+
 # сохраняем четверостишья в массив
 f = codecs.open(u'poems.txt', encoding=u'utf-8')
 poems = []
@@ -40,23 +43,34 @@ for line in f.readlines():
         s+= line
     else:
         if s:
-            s = delete.sub(' ', s)
-            s = re.sub(pattern, u' ', s).strip()
-            poems.append(s.lower())
+            poems.append(s)
             s = ''
 else:
     if s:
-        s = delete.sub(u' ', s)
-        s = re.sub(pattern, u' ', s).strip()
-        poems.append(s.lower())
+        poems.append(s)
 
+# построение и печать индекса
+p_index = do_index(poems)
+for key in sorted(p_index.keys()):
+    print u'{0:14} ==> {1}'.format(key, p_index[key])
+
+
+request = raw_input().decode(encoding='utf-8')
+request = clear(request)
+
+
+# полнотекстовый поиск
+# мне кажется, от него стоит вообще отказаться, иначе на большом объеме стихов мы зависнем, так и не дойдя до поиска
+# по индексу. вместо него лучше использовать вхождение нормальных форм этих слов с минимальным расстоянием
 for item in poems:
-    print item
+    temp = clear(item)
+    if u' ' not in request:
+        if request in temp.split():
+            print item
+    else:
+        if request in temp:
+            print item
 
-# p_index = do_index(poems)
-#
-# for key in sorted(p_index.keys()):
-#     print u'{0:14} ==> {1}'.format(key, p_index[key])
 
 
 
