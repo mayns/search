4cafeb3d8423db083343b4e975df6c636c7a2b5b
dmitrymakhotin <surfpandas@gmail.com>
2014-12-10 19:29:26 +0300

    clear poems with regex

--- index/index.py
+++ index/index.py
@@ -6,15 +6,18 @@ import codecs
 from normal_form import get_normal
 import re
 
+
+
 def do_index(l):
     """создает индекс. на вход - лист строк, каждая строка - отдельное четверостишье"""
-    delete = re.compile(u'\W+?|\d+?', re.UNICODE)
+    # delete = re.compile(u'\W+?|\d+?', re.UNICODE)
     poem_index = {}
     for i in xrange(len(l)):
         print 'creating index for poem ...', i
         pos = 0
-        sen = delete.sub(' ', l[i])
-        sen = sen.split()
+        # sen = delete.sub(' ', l[i])
+        # sen = sen.split()
+        sen = l[i].split()
         # for s in sen:
         #     print s
         for word in sen:
@@ -26,6 +29,8 @@ def do_index(l):
             pos +=1
     return poem_index
 
+delete = re.compile(r'\W+?|\d+?', re.UNICODE)
+pattern = re.compile(r'\s+', re.UNICODE)
 # сохраняем четверостишья в массив
 f = codecs.open(u'poems.txt', encoding=u'utf-8')
 poems = []
@@ -35,19 +40,23 @@ for line in f.readlines():
         s+= line
     else:
         if s:
-            poems.append(s)
+            s = delete.sub(' ', s)
+            s = re.sub(pattern, u' ', s).strip()
+            poems.append(s.lower())
             s = ''
 else:
     if s:
-        poems.append(s)
+        s = delete.sub(u' ', s)
+        s = re.sub(pattern, u' ', s).strip()
+        poems.append(s.lower())
 
-# for item in poems:
-#     print item
+for item in poems:
+    print item
 
-p_index = do_index(poems)
-
-for key in sorted(p_index.keys()):
-    print u'{0:14} ==> {1}'.format(key, p_index[key])
+# p_index = do_index(poems)
+#
+# for key in sorted(p_index.keys()):
+#     print u'{0:14} ==> {1}'.format(key, p_index[key])
 
 
 
